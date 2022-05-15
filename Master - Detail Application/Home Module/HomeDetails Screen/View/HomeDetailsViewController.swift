7769311212cdf431b05f5626916451b2acff5911
EsraaSliem <35599941+EsraaSliem@users.noreply.github.com>
2022-05-15 17:16:06 +0200

    Add HomeDetailsScreen

--- Master - Detail Application/Home Module/HomeDetails Screen/View/HomeDetailsViewController.swift
+++ Master - Detail Application/Home Module/HomeDetails Screen/View/HomeDetailsViewController.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
-
+import SDWebImage
 class HomeDetailsViewController: UIViewController {
-
+   @IBOutlet weak var moviesCollectionView: UICollectionView!
+    @IBOutlet weak var movieTitle: UINavigationItem!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var castLabel: UILabel!
+    var movie: Movie!
+    var viewModel: HomeDetailsViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initMVVM()
+        initView()
+        
+        
+    }
+    @IBAction func onPressedBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initView() {
+        yearLabel.text = "\(viewModel.movie.year)"
+        castLabel.text = viewModel.cast
+        genreLabel.text = viewModel.genres
+        movieTitle.title = viewModel.movie.title
+        configureCollectionView()
     }
-    */
-
+    
+    func initMVVM() {
+        viewModel = HomeDetailsViewModel(movie: movie)
+        viewModel.getMoviePhotos()
+        viewModel.reloadMoviesCollectionsViewClosure = {
+            self.moviesCollectionView.reloadData()
+        }
+    }
 }
+
+extension HomeDetailsViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func configureCollectionView() {
+         moviesCollectionView.registerCellNib(cellClass: MovieViewCell.self)
+         moviesCollectionView.delegate = self
+         moviesCollectionView.dataSource = self
+    }
+    
+    // MARK: - UICollectionViewDataSource protocol
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+        return viewModel.getPhotsCount()
+        
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeue(for: indexPath) as MovieViewCell
+        cell.movieImage.sd_setImage(with: URL(string: viewModel.getPhotoINdex(index: indexPath.row)))
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (collectionView.bounds.width - (2 + 1) * 8) / 2
+        return CGSize(width: size, height:  size)
+    }
+}
+
--- Master - Detail Application/Home Module/HomeDetails Screen/ViewModel/HomeDetailsViewModel.swift
+++ Master - Detail Application/Home Module/HomeDetails Screen/ViewModel/HomeDetailsViewModel.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+class HomeDetailsViewModel: NSObject {
+    //MARK:- Properties
+    let movie: Movie
+    var photos: [Photo] = [] {
+        didSet {
+            reloadMoviesCollectionsViewClosure?()
+        }
+    }
+    var genres: String {
+        return movie.genres.joined(separator:", ")
+    }
+    var cast: String {
+        return movie.cast.joined(separator:", ")
+    }
+    let services: HomeServicesProtocol
+    var reloadMoviesCollectionsViewClosure: (()->())?
+    
+    //MARK:- Init
+    init(services: HomeServicesProtocol = HomeServices(), movie: Movie) {
+        self.services = services
+        self.movie = movie
+    }
+    
+    //MARK:- Data Presentation
+    // handling cells and tableViewData from loaded data
+    func getPhotsCount() -> Int {
+        return photos.count
+    }
+    
+    func getPhotoINdex(index: Int) -> String {
+        return photos[index].url
+    }
+    
+    
+    //MARK:- Load Data
+    // get movies Photos from api
+    func getMoviePhotos() {
+        let request = createRequest()
+        services.getPhotos(request: request, sucess: {[weak self] photos in
+            self?.photos = photos
+        }, error: { error in
+            // TODO: -handling error
+            print(error)
+        })
+    }
+    
+    // grouping and categories data by year
+    func createRequest() -> PhotoSearchRequest {
+        return PhotoSearchRequest(title: movie.title, page: 1, perPage: 10)
+    }
+}
