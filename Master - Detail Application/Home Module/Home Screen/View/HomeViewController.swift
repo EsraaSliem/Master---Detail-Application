b8cf335c573cd6c5349398e5b5bbaf493933077b
EsraaSliem <35599941+EsraaSliem@users.noreply.github.com>
2022-05-15 17:15:32 +0200

    Add search to Movies

--- Master - Detail Application/Home Module/Home Screen/View/HomeViewController.swift
+++ Master - Detail Application/Home Module/Home Screen/View/HomeViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    //MARK:- IBOutlets
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    lazy var viewModel: HomeViewModel = {
+        return HomeViewModel()
+    }()
+    
+    //MARK:- viewDidLoad
     override func viewDidLoad() {
+        configureSearchBar()
+        configureTableView()
+        initMVVM()
         super.viewDidLoad()
+    }
+   
+    //MARK:- init MVVM
+    func initMVVM(){
+        viewModel.getMovies()
+        viewModel.reloadMoviesTableViewClosure = { [weak self] () in
+            DispatchQueue.main.async {
+                self?.homeTableView.reloadData()
+            }
+        }
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    //MARK:- init TableView
+    func configureTableView() {
+        
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK:- TableViewDelegation methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.getRowsInSectionCount(sectionNum: section)
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.getMoviesSectionCount()
+    }
+    
+    //MARK:- TableViewDataSource methods
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.getSectionHeader(sectionNum: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
+        cell.textLabel?.text = viewModel.getCellData(row: indexPath.row, sectionNum: indexPath.section).title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let homeDetailsViewController = storyBoard.instantiateViewController(withIdentifier: "HomeDetailsViewController") as! HomeDetailsViewController
+        homeDetailsViewController.movie = viewModel.movies[indexPath.row]
+        homeDetailsViewController.modalPresentationStyle = .fullScreen
+        self.present(homeDetailsViewController, animated: true, completion: nil)
+        
+    }
+}
 
+extension HomeViewController: UISearchBarDelegate {
+    func configureSearchBar() {
+        searchBar.delegate = self
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.searchMovies(searchText: searchText)
+    }
 }
--- Master - Detail Application/Home Module/Home Screen/ViewModel/HomeViewModel.swift
+++ Master - Detail Application/Home Module/Home Screen/ViewModel/HomeViewModel.swift
@@ -6,3 +6,93 @@
 //
 
 import Foundation
+class HomeViewModel: NSObject {
+    //MARK:- Properties
+    var movies: [Movie] = [] {
+        didSet {
+            filterMovies = movies
+        
+        }
+    }
+    
+    var filterMovies: [Movie] = []
+    var reloadMoviesTableViewClosure: (()->())?
+    var currentMovies: [Movie] = []
+    
+    var categoriesMovies: Dictionary<Int, [Movie]> = [:] {
+        didSet {                reloadMoviesTableViewClosure?()
+        }
+    }
+    
+    private let services: HomeServicesProtocol
+    
+    
+    //MARK:- Init
+    init(services: HomeServicesProtocol = HomeServices()) {
+        self.services = services
+    }
+    
+    //MARK:- Data Presentation
+    // handling cells and tableViewData from loaded data
+    func getMoviesSectionCount() -> Int {
+        return categoriesMovies.count
+    }
+    
+    func getCellData(row: Int, sectionNum: Int) -> Movie
+    {
+        currentMovies = Array(categoriesMovies)[sectionNum].value.sorted(by: { $0.rating > $1.rating })
+        return currentMovies[row]
+    }
+    
+    func getRowsInSectionCount(sectionNum: Int) -> Int
+    {
+        let count = Array(categoriesMovies)[sectionNum].value.count
+      
+        return count > 5 ? 5 : count
+    }
+    
+    func getSectionHeader(sectionNum: Int) -> String
+    {
+        return "\(Array(categoriesMovies.keys)[sectionNum])"
+    }
+    
+    
+    //MARK:- Load Data
+    // get movies from api
+    func getMovies() {
+        guard let movies = services.loadMovies() else { return }
+        self.movies = movies
+        categoriesMovie()
+        
+    }
+    
+    // grouping and categories data by year
+    func categoriesMovie() {
+        let categoriesedMovieList  = Dictionary(grouping: movies) { $0.year }
+        self.categoriesMovies = categoriesedMovieList
+     
+    }
+    
+    func categoriesSortedMovie() {
+        let categoriesedMovieList  = Dictionary(grouping: filterMovies.sorted(by: {$0.year > $1.year })) { $0.year }
+        
+        
+        self.categoriesMovies = categoriesedMovieList
+        
+    }
+    
+    func searchMovies(searchText: String) {
+        if searchText.isEmpty {
+            filterMovies = movies
+            categoriesMovie()
+        }
+        else  {
+            filterMovies =  movies.filter({$0.title.contains(searchText)})
+            categoriesSortedMovie()
+        }
+ 
+        
+    }
+}
+
+
