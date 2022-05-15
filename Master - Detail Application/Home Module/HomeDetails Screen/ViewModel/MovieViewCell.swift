e07f0daab05267bb22bba828ed5a770a946cc571
EsraaSliem <35599941+EsraaSliem@users.noreply.github.com>
2022-05-15 17:12:12 +0200

    Update UI
    - update Home details View
    - add MovieCell

---
+++ Master - Detail Application/Home Module/HomeDetails Screen/ViewModel/MovieViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieViewCell.swift
+//  Master - Detail Application
+//
+//  Created by Esraa Sliem on 15/05/2022.
+//
+
+import UIKit
+
+class MovieViewCell: UICollectionViewCell {
+    @IBOutlet weak var movieImage: UIImageView!
+}
